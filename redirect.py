ca84a86c5e575641a8c9c9a5f3da01be62837c13
Davis Agyemang <davis.agyemang@co-cc04647.ad.cabinetoffice.gov.uk>
2026-01-27 15:01:56 +0000

    updated code to pass in website link to new CI chat

--- redirect.py
+++ redirect.py
@@ -1,12 +1,16 @@
 from flask import Flask, render_template
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
 app = Flask(__name__)
 
+redirect_link = os.getenv("REDIRECT_LINK")
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', redirect_link=redirect_link)
 
 if __name__ == '__main__':
     app.run(debug=True)
